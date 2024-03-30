3d969599125378cb1e745c5f93efbe3e94c14067
krzychit <80640964+krzychit@users.noreply.github.com>
2024-03-30 22:06:37 +0100

    Add files via upload
    I have a problem getting the parameters. I plan to separate the calculator calculation part and the credit calculator part.

---
+++ app/podstrona_secure.php
@@ -0,0 +1,25 @@
+<?php
+require_once dirname(__FILE__).'/../config.php';
+//ochrona widoku
+include _ROOT_PATH.'/app/securityapp/check.php';
+?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<title>Chroniona strona</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css"">
+</head>
+<body>
+
+<div style="width:90%; margin: 2em auto;">
+	<a href="<?php print(_APP_ROOT); ?>/app/calc.php" class="pure-button">Powrót do kalkulatora</a>
+	<a href="<?php print(_APP_ROOT); ?>/app/securityapp/logout.php" class="pure-button pure-button-active">Wyloguj</a>
+</div>
+
+<div style="width:90%; margin: 2em auto;">
+	<p>To jest inna chroniona strona aplikacji internetowej</p>
+</div>	
+
+</body>
+</html>
